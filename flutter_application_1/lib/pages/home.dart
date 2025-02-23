4e4eeee993263b8bafc434ac609cfd74793df0d9
kofi527 <63665356+kofi527@users.noreply.github.com>
2025-02-23 03:55:15 -0600

    home page update
    shaping and  organizing images in proper columns

--- flutter_application_1/lib/pages/home.dart
+++ flutter_application_1/lib/pages/home.dart
@@ -13,7 +13,7 @@ class _OnboardingState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        margin: EdgeInsets.only(top: 40.0, left: 20.0),
+            margin: EdgeInsets.only(top: 50.0, left: 20.0, right: 20.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [ 
