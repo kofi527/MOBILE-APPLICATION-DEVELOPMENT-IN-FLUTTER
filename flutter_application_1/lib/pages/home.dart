f2e0b4d27b0d13dbc857256333d13f45ea9ad373
kofi527 <63665356+kofi527@users.noreply.github.com>
2025-02-23 04:39:16 -0600

    Update home.dart
    creating additional text and column below the introduction image

--- flutter_application_1/lib/pages/home.dart
+++ flutter_application_1/lib/pages/home.dart
@@ -12,6 +12,7 @@ class _OnboardingState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xffF5F5F5),
       body: Container(
             margin: EdgeInsets.only(top: 50.0, left: 20.0, right: 20.0),
         child: Column(
@@ -20,15 +21,30 @@ class _OnboardingState extends State<Home> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-        Text("Hey, Shivam", 
-        style: AppWidget.boldTextFeildStyle(),
-        ),
-          Image.asset("images/boy.jpg", height: 70, width: 70, fit: BoxFit.cover,)
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                 children: [
+                    Text(
+                      "Hey, Shivam", 
+                      style: AppWidget.boldTextFeildStyle(),
+                ),
+                      Text(
+                        "Good Morning", 
+                       style: AppWidget.lightTextFeildStyle(),
+          ),
+              ],
+            ),
+                   ClipRRect(
+                    borderRadius: BorderRadius.circular(30.0),
+                    child: Image.asset("images/boy.jpg", height: 70, width: 70, fit: BoxFit.cover,))
           ],
           ),
-        Text("Good Morning", 
-        style: AppWidget.lightTextFeildStyle(),
-          ),
+  Container(
+    decoration: BoxDecoration(color: Colors.white),
+    width: MediaQuery.of(context).size.width,
+    child: TextField(decoration: InputDecoration(border: InputBorder.none),
+    )
+  )
           
 
       ],
