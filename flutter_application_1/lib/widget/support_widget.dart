cd3ceacede194ea44f010471d0bec2b401c259c7
kofi527 <63665356+kofi527@users.noreply.github.com>
2025-03-03 14:52:10 -0600

    Update support_widget.dart
    Create a static class for the text field "categories" that can be re- used in other sections

--- flutter_application_1/lib/widget/support_widget.dart
+++ flutter_application_1/lib/widget/support_widget.dart
@@ -14,4 +14,10 @@ class AppWidget{
           color: Colors.black45, 
           fontSize: 20.0, fontWeight: FontWeight.w500);
     }
+    static TextStyle semiboldTextFeildStyle() {
+        return TextStyle(
+            color: Colors.black, 
+            fontSize: 20.0, 
+            fontWeight: FontWeight.bold);
+    }
 }
